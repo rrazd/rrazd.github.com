f9c97bdb780f4162f7d2133a239afd8e1ce2c30f
Rashna Razdan <rashnarazdan@gmail.com>
2016-07-03 08:43:16 -0400

    sticky hover fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,16 @@
 	function initialize () {
 		new Nav().initialize();
 	}
-
 	document.addEventListener('DOMContentLoaded', function() {
 		initialize();
 	}, false);
 
 })();
+/* Needed for fixing sticky hover in mobile */
+function fix() {
+    var el = this;
+    var par = el.parentNode;
+    var next = el.nextSibling;
+    par.removeChild(el);
+    setTimeout(function() {par.insertBefore(el, next);}, 0);
+}
