77bdc0729a0b009dc76ad25c37138e8959b8beb9
Rashna Razdan <rashnarazdan@gmail.com>
2017-03-14 00:33:06 -0700

    working homepage and start of nexJ caseStudy

---
+++ assets/js/caseStudy.js
@@ -0,0 +1,13 @@
+$(window).scroll(function() {
+	var scroll = $(window).scrollTop();
+	if (scroll >= 30) {
+        $(".case-header").addClass("shade");
+    } else {
+    	$(".case-header").removeClass("shade");
+    }
+});
+
+$(document).ready(function() {
+	// once the javascript is loaded and styles added, hide the loader
+	$('.loader').addClass("hidden");
+});
